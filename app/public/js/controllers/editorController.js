634e71f398a26ca4cb6d88e83d9744928934e7b4
Glavin Wiechert <Glavin.Wiechert@gmail.com>
2013-11-11 18:06:14 -0400

    See #14. Map builder now has external window, loads existing GeoJSON data, and uses CodeMirror for editor.

--- app/public/js/controllers/editorController.js
+++ app/public/js/controllers/editorController.js
@@ -1,8 +1,10 @@
 (function( editorController, undefined ) {
 	var self = editorController;
+	var indentSpaces = 2;
 
 	self.initParent = function() {
-		var myWindow = window.open("/builder-editor","_blank","width=500,height=500");
+		var myWindow = window.open("/builder-editor","_blank",
+			"width=500,height=500,menubar=no,status=no,resizable=yes,titlebar=yes,toolbar=no");
 		//myWindow.document.write("<p>This is 'myWindow'</p>");
 		//myWindow.focus();
 		//myWindow.opener.document.write("<p>This is the source window!</p>");
@@ -11,21 +13,37 @@
 
 	self.initChild = function() {
 		console.log('Init child with parent: ', window.opener);
-		
-		var editor = CodeMirror.fromTextArea($("#codeEditor").get(0), {
+		var $el = $("#codeEditor");
+		var editor = CodeMirror.fromTextArea($el.get(0), {
 			mode: "application/json",
 			lineNumbers: true,
 			matchBrackets: true,
 			continueComments: "Enter",
-			indentUnit: 4,
+			indentUnit: indentSpaces,
 			smartIndent: true,
-			tabSize: 4,
+			tabSize: indentSpaces,
 			indentWithTabs: true,
 			lineWrapping: true,
 			tabMode: "indent",
-			viewportMargin: Infinity
+			viewportMargin: Infinity,
+			extraKeys: { "Ctrl-Q": "toggleComment" }
 		});
 
+		$.getJSON("/data/campus.geojson", function(geojsonFeature) {
+			var str = JSON.stringify(geojsonFeature, undefined, indentSpaces);
+			editor.setValue(str);
+		});
+
+		function resizeEditor() {
+			var height = $(window).height() - $('.tab-content').offset().top - 1;
+			$('.tab-content').height(height);
+		};
+		var resizeTimer;
+		$(window).resize(function() {
+		    clearTimeout(resizeTimer);
+		    resizeTimer = setTimeout(resizeEditor, 100);
+		});
+		resizeEditor(); // Resize
 	};
 
 })(window.editorController = window.editorController || { } );
