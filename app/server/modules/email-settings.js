80f620bd02573415f25abc2ead9752186c0d5d2a
danlock <d.f.locked@gmail.com>
2013-12-31 23:26:31 -0500

    added beta twilio functionality #10

--- app/server/modules/email-settings.js
+++ app/server/modules/email-settings.js
@@ -1,9 +1,7 @@
 
 module.exports = {
-	
 	host		: 'smtp.gmail.com',
 	user 		: 'your-email-address@gmail.com',
 	password 	: 'your-email-password',
 	sender		: 'Your Name <your-email-address@gmail.com>'
-	
 }
