207860c192b5a48a4152ee5e85a35f4e424d89ae
danlock <d.f.locked@gmail.com>
2014-02-19 00:14:23 -0400

    fixed form data #54

--- app/server/modules/faculty-search.js
+++ app/server/modules/faculty-search.js
@@ -4,10 +4,12 @@ var FD = FormData();
 module.exports = function (fname,lname) {
 	FD.append('txtFirstName',fname);
 	FD.append('txtLastName',lname);
+	FD.append('cmdSubmit','Submit');
 
 	XHR.addEventListener('load', function (e) {
 		console.log("Sent XHR request successfully");
 	});
+
 	XHR.addEventListener('error', function (e) {
 		console.log("Sent XHR request, got error");
 	});
