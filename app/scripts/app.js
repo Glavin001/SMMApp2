d8c61fece77454a20dc449f6a948412dbf6d363a
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-03-30 13:43:04 -0300

    See #60. Multiple routes and separate navbar and footer.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,6 +13,18 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/account', {
+        templateUrl: 'views/account.html',
+        controller: 'AccountCtrl'
+      })
+      .when('/map', {
+        templateUrl: 'views/map.html',
+        controller: 'MapCtrl'
+      })
+      .when('/events', {
+        templateUrl: 'views/events.html',
+        controller: 'EventsCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/scripts/controllers/account.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('smmapp2App')
+  .controller('AccountCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
