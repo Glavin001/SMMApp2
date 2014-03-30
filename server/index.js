1aa91eade64432d70ca7a16782c20b29593b5817
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-03-30 16:42:15 -0300

    See #61. Started Faculty Search service.

--- server/index.js
+++ server/index.js
@@ -1,24 +1,16 @@
+// Dependencies
 var feathers = require('feathers');
- 
-var todoService = {
-  todos: [],
-  
-  // Return all todos from this service
-  find: function(params, callback) {
-    callback(null, this.todos);
-  },
-  
-  // Create a new Todo with the given data
-  create: function(data, params, callback) {
-    data.id = this.todos.length;
-    this.todos.push(data);
-    
-    callback(null, data);
-  }
-};
- 
+
+// Services
+var facultySearchService = require('./services/facultySearch');
+
+// Config
+var port = 8080;
+
+// Start server
 feathers()
     .configure(feathers.socketio())
-    .use('/todos', todoService)
-    .listen(8000);
-    
+    .use('/faculty', facultySearchService)
+    .listen(port, function() {
+        console.log('Listening on port '+port+'.');
+    });
